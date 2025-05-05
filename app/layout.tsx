e77c427ce4a48244e459d17316e8382d328203cc
zwroe <166211906+zwroee@users.noreply.github.com>
2025-05-05 01:59:21 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "About Me",
-  description: "My personal about me page",
+  title: "zwroe",
+  description: "⚡",
   icons: {
     icon: "/favicon.ico",
     apple: "/apple-icon.png",
