c98718bbc276e79ee34fd2d8ba5adbcc74a928e2
zwroe <166211906+zwroee@users.noreply.github.com>
2025-05-05 20:22:17 -0400

    Create head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/favicon.png" />
+      <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+    </>
+  )
+}
